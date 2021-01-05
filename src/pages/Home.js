bd68416e365d392476eca06d73ed116d6cf36a2d
ridgereventar <ridge.reventar@gmail.com>
2021-01-05 16:00:49 -0500

    Home page setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import '../styles/Home.css';
 
+import logowhite from '../images/ridgelogo_white.png'
+
 const Home = (props) => {
     return (
-        <h1>home</h1>
+        <div className="home">
+            <div className="nav-bar">
+                <img id="navlogo" src={logowhite}></img>
+                <ul className="nav">
+                    <li className="nav-link">About</li>
+                    <li className="nav-link">Projects</li>
+                    <li className="nav-link">Contact</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 

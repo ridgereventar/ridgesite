5aadb9eb1a8ab93954363bae41fd3b6b5fd71c5c
ridgereventar <ridge.reventar@gmail.com>
2021-06-07 12:30:48 -0400

    fixed anchor tag warning

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -5,11 +5,15 @@ import '../styles/styles.css';
 function Socials() {
     return (
         <div className="socials">
-            <a id="mail" className="social-icon" href="mailto:ridge.reventar@gmail.com"/>
+            <a id="mail" className="social-icon" href="mailto:ridge.reventar@gmail.com"> 
+                <span>&#160;</span>
+            </a>
             {/* <div id="ig" className="social-icon"></div> */}
             <a id="linked" className="social-icon" href="https://www.linkedin.com/in/ridge-reventar-76a12a214/" target="_blank" rel="noreferrer">
+                <span>&#160;</span>
             </a>
             <a id="git" className="social-icon" href="https://github.com/ridgereventar" target="_blank" rel="noreferrer">
+                <span>&#160;</span>
             </a>
         </div>
     )

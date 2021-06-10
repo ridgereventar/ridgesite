62b4b487d1e11d21e72413d1675cdddc78c4d339
ridgereventar <ridge.reventar@gmail.com>
2021-06-10 18:24:10 -0400

    Updated linked in link

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -9,7 +9,7 @@ const Socials = ({dark}) => {
                 <span>&#160;</span>
             </a>
             {/* <div id="ig" className="social-icon"></div> */}
-            <a id="linked" className={`social-icon ${dark? "dark-icon" : ""}`} href="https://www.linkedin.com/in/ridge-reventar-76a12a214/" target="_blank" rel="noreferrer">
+            <a id="linked" className={`social-icon ${dark? "dark-icon" : ""}`} href="https://www.linkedin.com/in/ridgereventar/" target="_blank" rel="noreferrer">
                 <span>&#160;</span>
             </a>
             <a id="git" className={`social-icon ${dark? "dark-icon" : ""}`} href="https://github.com/ridgereventar" target="_blank" rel="noreferrer">
